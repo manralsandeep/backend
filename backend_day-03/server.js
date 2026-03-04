d75f32fad3893a4e7a0e08aa18ddb8e7b5650825
manralsandeep <manralsandeep038@gmail.com>
2026-03-04 14:45:55 +0530

    modified code

--- backend_day-03/server.js
+++ backend_day-03/server.js
@@ -4,34 +4,5 @@ const express = require('express')
 
 const app = require('./src/app')
 
-app.use(express.json())
 
-const notes = []
-
-app.get('/notes', (req, res) => {
-   res.send(notes)
-})
-
-app.post('/notes', (req, res) => {
-    notes.push(req.body)
-    res.send('note created')
-})
-
-app.delete('/notes/:index', (req, res) => {
-   
-    delete notes[req.params.index]
-    res.send('note deleted sucesfully')
-})
-
-app.patch('/notes/:index', (req, res) => {
-    notes[req.params.index].description=req.body.description
-    res.send('note modified sucesfully')
-})
-
-app.put('/notes/:index', (req, res) => {
-    notes[req.params.index].tittle= req.body.tittle
-    notes[req.params.index].description = req.body.description
-    res.send('note modified sucesfully')
-    
-})
 app.listen(3000, () => { console.log('server running at port 3000') })  //server started
--- backend_day-03/src/app.js
+++ backend_day-03/src/app.js
@@ -2,6 +2,35 @@ const express = require('express')
 const app = express()  //server created
 
 //server config krna
+app.use(express.json())
 
+const notes = []
+
+app.get('/notes', (req, res) => {
+   res.send(notes)
+})
+
+app.post('/notes', (req, res) => {
+    notes.push(req.body)
+    res.send('note created')
+})
+
+app.delete('/notes/:index', (req, res) => {
+   
+    delete notes[req.params.index]
+    res.send('note deleted sucesfully')
+})
+
+app.patch('/notes/:index', (req, res) => {
+    notes[req.params.index].description=req.body.description
+    res.send('note modified sucesfully')
+})
+
+app.put('/notes/:index', (req, res) => {
+    notes[req.params.index].tittle= req.body.tittle
+    notes[req.params.index].description = req.body.description
+    res.send('note modified sucesfully')
+    
+})
 module.exports=app
  
